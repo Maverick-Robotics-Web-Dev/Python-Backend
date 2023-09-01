952d3fae9d1060967e7bd3aed2e77ca47cc280dc
Maverick <mavroboticswebdev7690@gmail.com>
2023-09-01 15:40:36 -0500

    Create Api

---
+++ Django_Backend/drf_simple_crud/webshop_mavtronics_test/app/db_config.py
@@ -0,0 +1,10 @@
+MYSQL={
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'NAME': 'webshop_mavtronics_test',
+        'USER': 'root',
+        'PASSWORD': 'Wilmar2023'
+    }
+}
