6d18deef8108c70a93f9d57ff45e433ec324c45a
Maverick <mavroboticswebdev7690@gmail.com>
2023-09-05 17:37:29 -0500

    Create Serializers

--- Django_Backend/drf_simple_crud/webshop_mavtronics_test/restapi/serializers.py
+++ Django_Backend/drf_simple_crud/webshop_mavtronics_test/restapi/serializers.py
@@ -5,7 +5,6 @@ from .models import UserAdmin
 class UserAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAdmin
-    fields = ("user_admin_id", "user_admin_name", "user_admin_lastname", "user_admin_user_name", "user_admin_password",
-              "user_admin_login", "user_admin_status", "user_admin_status_description", "user_admin_create_at", "user_admin_update_at",)
-    read_only_fields = (
-        "user_admin_id", "user_admin_create_at", "user_admin_update_at")
+        fields = ['user_admin_id', 'user_admin_name', 'user_admin_lastname', 'user_admin_user_name', 'user_admin_password',
+                  'user_admin_login', 'user_admin_status', 'user_admin_status_description', 'user_admin_create_at', 'user_admin_update_at']
+        read_only_fields = ['user_admin_id']
