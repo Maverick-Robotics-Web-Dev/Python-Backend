a7f25b0c4420162178c1ef4a04ce172d84269aeb
Maverick <mavroboticswebdev7690@gmail.com>
2023-11-21 12:00:24 -0500

    Delete All

--- Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Seccion_4/empleado/empleado/wsgi.py
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Seccion_4/empleado/empleado/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test_app project.
+WSGI config for empleado project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_app.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empleado.settings.local')
 
 application = get_wsgi_application()
