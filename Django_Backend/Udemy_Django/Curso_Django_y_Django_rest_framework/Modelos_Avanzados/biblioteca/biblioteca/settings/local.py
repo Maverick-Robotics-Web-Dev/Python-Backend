941b9f0389ee5717eb8317a31f26adf86d449253
Maverick <mavroboticswebdev7690@gmail.com>
2023-11-21 17:39:15 -0500

    Add Udemy Course

---
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/biblioteca/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+        'USER' : 'postgres',
+        'PASSWORD' : 'ChrisChaV7690',
+        'NAME': 'dbbiblioteca',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
---
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/autor/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=30)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name}-{self.surname}'
