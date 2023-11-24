9ac7501f2a1190620e486801833f288186a67c30
Chris <mavroboticswebdev7690@gmail.com>
2023-11-23 23:27:38 -0500

    Creation of the other models

--- Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/autor/models.py
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/autor/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    nationality = models.CharField(max_length=30)
+    name = models.CharField("Nombre",max_length=50)
+    surname = models.CharField("Apellido",max_length=50)
+    nationality = models.CharField("Nacionalidad",max_length=30)
     age = models.PositiveIntegerField()
 
     def __str__(self):
--- Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/lector/models.py
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/lector/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from modules.libro.models import Book
 
 # Create your models here.
+
+class Reader(models.Model):
+    names = models.CharField("Nombres", max_length=50)
+    surnames = models.CharField("Apellidos", max_length=50)
+    nationality = models.CharField("Nacionalidad", max_length=20)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.names
+    
+class Loan(models.Model):
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    loan_date = models.DateField()
+    come_back_date = models.DateField(blank=True, null=True)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.book.title
