ccaa67ff2cf663a13fa1c95a5200bfaaa9fecb37
Chris <mavroboticswebdev7690@gmail.com>
2023-11-27 22:30:47 -0500

    Register Models in Admin

--- Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/autor/admin.py
+++ Django_Backend/Udemy_Django/Curso_Django_y_Django_rest_framework/Modelos_Avanzados/biblioteca/modules/autor/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from models import Author
+from .models import Author
 
 # Register your models here.
 
-admin.site.register(Author)
+admin.site.register([Author])
