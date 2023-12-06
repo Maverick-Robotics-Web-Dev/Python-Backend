0996cc04c2900b25ab278383c1fef4468268575c
Maverick <mavroboticswebdev7690@gmail.com>
2023-12-06 16:52:41 -0500

    Create Models Folder

--- Django_Backend/drf_webshop_valamy/webshop_resapi/webshop_resapi/settings/local.py
+++ Django_Backend/drf_webshop_valamy/webshop_resapi/webshop_resapi/settings/local.py
@@ -11,8 +11,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'NAME': 'webshop_valamy_test',
+        'USER': 'root',
+        'PASSWORD': 'ChrisChaV7690'
     }
 }
 
