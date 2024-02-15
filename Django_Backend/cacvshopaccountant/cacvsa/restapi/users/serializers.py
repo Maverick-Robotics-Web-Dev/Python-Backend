a2bfd092e8f8f92532d89e368ac7d4868d20f867
Chris <mavroboticswebdev7690@gmail.com>
2024-02-15 17:16:56 -0500

    Finished all Viewsets

---
+++ Django_Backend/cacvshopaccountant/cacvsa/restapi/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class UserLevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserLevelModel
+        fields = '__all__'
+        read_only_fields = ['user_level_id']
+
+
+class UserEmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserEmployeeModel
+        fields = '__all__'
+        read_only_fields = ['user_employee_id']
+
+
+class UserClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserClientModel
+        fields = '__all__'
+        read_only_fields = ['user_client_id']
--- Django_Backend/cacvshopaccountant/cacvsa/restapi/users/views.py
+++ Django_Backend/cacvshopaccountant/cacvsa/restapi/users/views.py
@@ -1,3 +1,181 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework.status import *
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class UserLevelViewSet(GenericViewSet):
+
+    model = UserLevelModel
+    serializer_class = UserLevelSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_queryset(self):
+        if self.queryset is None:
+            return self.model.objects.filter(user_level_status=True)
+        return self.queryset
+
+    def list(self, request):
+        # print(request)
+        queryres = self.get_queryset()
+        serializer = self.serializer_class(queryres, many=True)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        # print(pk)
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(queryres)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def create(self, request):
+        # print(request.data)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(
+            queryres, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        queryres = self.model.objects.filter(
+            user_level_id=pk).update(user_level_status=False)
+        if queryres == 1:
+            data = {'msg': 'OK'}
+            return Response(data, HTTP_200_OK)
+        data = {'msg': 'ERROR'}
+        return Response(data, HTTP_404_NOT_FOUND)
+
+
+class UserEmployeeViewSet(GenericViewSet):
+
+    model = UserEmployeeModel
+    serializer_class = UserEmployeeSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_queryset(self):
+        if self.queryset is None:
+            return self.model.objects.filter(is_active=True)
+        return self.queryset
+
+    def list(self, request):
+        # print(request)
+        queryres = self.get_queryset()
+        serializer = self.serializer_class(queryres, many=True)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        # print(pk)
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(queryres)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def create(self, request):
+        # print(request.data)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(
+            queryres, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        queryres = self.model.objects.filter(
+            id=pk).update(is_active=False)
+        if queryres == 1:
+            data = {'msg': 'OK'}
+            return Response(data, HTTP_200_OK)
+        data = {'msg': 'ERROR'}
+        return Response(data, HTTP_404_NOT_FOUND)
+
+
+class UserClientViewSet(GenericViewSet):
+
+    model = UserClientModel
+    serializer_class = UserClientSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_queryset(self):
+        if self.queryset is None:
+            return self.model.objects.filter(user_client_status=True)
+        return self.queryset
+
+    def list(self, request):
+        # print(request)
+        queryres = self.get_queryset()
+        serializer = self.serializer_class(queryres, many=True)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        # print(pk)
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(queryres)
+        data = {'msg': 'OK', 'data': serializer.data}
+        return Response(data, HTTP_200_OK)
+
+    def create(self, request):
+        # print(request.data)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        queryres = self.get_object(pk)
+        serializer = self.serializer_class(
+            queryres, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data = {'msg': 'OK', 'data': serializer.data}
+            return Response(data, HTTP_201_CREATED)
+        data = {'msg': 'ERROR', 'errors': serializer.errors}
+        return Response(data, HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        queryres = self.model.objects.filter(
+            user_client_id=pk).update(user_client_status=False)
+        if queryres == 1:
+            data = {'msg': 'OK'}
+            return Response(data, HTTP_200_OK)
+        data = {'msg': 'ERROR'}
+        return Response(data, HTTP_404_NOT_FOUND)
