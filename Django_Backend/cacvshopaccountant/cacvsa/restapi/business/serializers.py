719c6a21a8c0f1d0347f4836023cff2bf8f78bc3
Chris <mavroboticswebdev7690@gmail.com>
2024-03-15 17:18:50 -0500

    Still edit

--- Django_Backend/cacvshopaccountant/cacvsa/restapi/business/serializers.py
+++ Django_Backend/cacvshopaccountant/cacvsa/restapi/business/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer, StringRelatedField
 
 from .models import *
-from restapi.users.serializers import UserEmployeeSerializer
+# from restapi.users.serializers import UserEmployeeSerializer
 
 
 class WayToPaySerializer(ModelSerializer):
@@ -9,9 +9,16 @@ class WayToPaySerializer(ModelSerializer):
     # Method 1
     # fk_user_employee = UserEmployeeSerializer()
     # Method 2
+    fk_user_employee: StringRelatedField = None
+
     fk_user_employee = StringRelatedField()
 
     class Meta:
+
+        model: WayToPayModel = None
+        fields: str | list[str] = None
+        read_only_fields: list[str] = None
+
         model = WayToPayModel
         fields = '__all__'
         read_only_fields = ['id']
@@ -19,9 +26,16 @@ class WayToPaySerializer(ModelSerializer):
 
 class VoucherTypeSerializer(ModelSerializer):
 
+    fk_user_employee: StringRelatedField = None
+
     fk_user_employee = StringRelatedField()
 
     class Meta:
+
+        model: VoucherTypeModel = None
+        fields: str | list[str] = None
+        read_only_fields: list[str] = None
+
         model = VoucherTypeModel
         fields = '__all__'
         read_only_fields = ['id']
@@ -29,10 +43,18 @@ class VoucherTypeSerializer(ModelSerializer):
 
 class CreditNoteSerializaer(ModelSerializer):
 
+    fk_client: StringRelatedField = None
+    fk_user_employee: StringRelatedField = None
+
     fk_client = StringRelatedField()
     fk_user_employee = StringRelatedField()
 
     class Meta:
+
+        model: CreditNoteModel = None
+        fields: str | list[str] = None
+        read_only_fields: list[str] = None
+
         model = CreditNoteModel
         fields = '__all__'
         read_only_fields = ['id']
@@ -40,11 +62,18 @@ class CreditNoteSerializaer(ModelSerializer):
 
 class CreditNoteDetailSerializer(ModelSerializer):
 
+    fk_credit_note: StringRelatedField = None
+    fk_product: StringRelatedField = None
+
     fk_credit_note = StringRelatedField()
     fk_product = StringRelatedField()
-    fk_user_employee = StringRelatedField()
 
     class Meta:
+
+        model: CreditNoteDetailModel = None
+        fields: str | list[str] = None
+        read_only_fields: list[str] = None
+
         model = CreditNoteDetailModel
         fields = '__all__'
         read_only_fields = ['id']
